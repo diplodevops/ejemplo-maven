10d3a6e335bcf36d81905c9a1af62952bfc1ac95
dptamayo22 <dptamayo22@gmail.com>
2020-11-30 21:58:29 -0300

    duplicando codigo para que falle sonar

--- src/main/java/com/devopsusach2020/rest/RestData.java
+++ src/main/java/com/devopsusach2020/rest/RestData.java
@@ -64,6 +64,37 @@ public class RestData {
 
 		return response;		
 	}
+
+	@GetMapping(path = "/estadoPais", produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Pais getTotalPais2(@RequestParam(name = "pais") String message){
+		RestTemplate restTemplate = new RestTemplate();
+	    ResponseEntity<String> call= restTemplate.getForEntity("https://api.covid19api.com/live/country/" + message ,String.class);
+
+	    LOGGER.log(Level.INFO, "Consulta por pais");
+
+		Pais response = new Pais();
+		int confirmed = 0;
+		int death = 0;
+		int recovered = 0;
+		Gson gson = new Gson();
+        Pais[] estados = gson.fromJson(call.getBody().toLowerCase(), Pais[].class);
+
+        for(Pais estado : estados) {
+        	response.setDate(estado.getDate());
+        	response.setActive(estado.getActive());
+        	confirmed += estado.getConfirmed();
+        	death += estado.getDeaths();
+        	recovered += estado.getRecovered();
+        }
+
+    	response.setConfirmed(confirmed);
+    	response.setDeaths(death);
+    	response.setRecovered(recovered);
+    	response.setCountry(message);
+    	response.setMensaje("ok");
+
+		return response;
+	}
 	
 
 	@GetMapping(path = "/estadoMundial", produces = MediaType.APPLICATION_JSON_VALUE)
