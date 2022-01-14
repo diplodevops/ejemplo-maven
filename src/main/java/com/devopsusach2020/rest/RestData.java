8ac95da285a4f33eec0987615bff51135f944418
luismella <luismellareyes@gmail.com>
2022-01-13 21:49:10 -0300

    sonarqube

--- src/main/java/com/devopsusach2020/rest/RestData.java
+++ src/main/java/com/devopsusach2020/rest/RestData.java
@@ -19,67 +19,85 @@ import com.google.gson.Gson;
 @RestController
 @RequestMapping(path = "/rest/mscovid")
 public class RestData {
-	
-	private final static Logger LOGGER = Logger.getLogger("devops.subnivel.Control");
 
-	
-	@GetMapping(path = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody Pais getData(@RequestParam(name = "msg") String message){
-		
-		LOGGER.log(Level.INFO, "Proceso exitoso de prueba");
-		
-		Pais response = new Pais();
-		response.setMensaje("Mensaje Recibido: " + message);
-		return response;
-	}
-	
-	
-	@GetMapping(path = "/estadoPais", produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody Pais getTotalPais(@RequestParam(name = "pais") String message){
-		RestTemplate restTemplate = new RestTemplate();
-	    ResponseEntity<String> call= restTemplate.getForEntity("https://api.covid19api.com/live/country/" + message ,String.class);
-	    
-	    LOGGER.log(Level.INFO, "Consulta por pais");
-	    
-		Pais response = new Pais();
-		int confirmed = 0;
-		int death = 0;
-		int recovered = 0;
-		Gson gson = new Gson();
-        Pais[] estados = gson.fromJson(call.getBody().toLowerCase(), Pais[].class);
+    private static final Logger LOGGER = Logger.getLogger("devops.subnivel.Control");
 
-        for(Pais estado : estados) {
-        	response.setDate(estado.getDate());
-        	response.setActive(estado.getActive());
-        	confirmed += estado.getConfirmed();
-        	death += estado.getDeaths();
-        	recovered += estado.getRecovered();
+
+    @GetMapping(path = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    Pais getData(@RequestParam(name = "msg") String message) {
+
+        LOGGER.log(Level.INFO, "Proceso exitoso de prueba");
+
+        Pais response = new Pais();
+        response.setMensaje("Mensaje Recibido: " + message);
+        return response;
+    }
+
+
+    @GetMapping(path = "/estadoPais", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    Pais getTotalPais(@RequestParam(name = "pais") String message) {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> call = restTemplate.getForEntity("https://api.covid19api.com/live/country/" + message, String.class);
+
+        LOGGER.log(Level.INFO, "Consulta por pais");
+
+        Pais response = new Pais();
+        int confirmed = 0;
+        int death = 0;
+        int recovered = 0;
+        Gson gson = new Gson();
+
+
+        String body = call.getBody();
+        if (body != null) {
+
+            Pais[] estados = gson.fromJson(body.toLowerCase(), Pais[].class);
+
+            for (Pais estado : estados) {
+                response.setDate(estado.getDate());
+                response.setActive(estado.getActive());
+                confirmed += estado.getConfirmed();
+                death += estado.getDeaths();
+                recovered += estado.getRecovered();
+            }
+
+            response.setConfirmed(confirmed);
+            response.setDeaths(death);
+            response.setRecovered(recovered);
+            response.setCountry(message);
+            response.setMensaje("ok");
+
+            return response;
         }
+        return new Pais();
         
-    	response.setConfirmed(confirmed);
-    	response.setDeaths(death);
-    	response.setRecovered(recovered);
-    	response.setCountry(message);
-    	response.setMensaje("ok");
+    }
 
-		return response;		
-	}
-	
 
-	@GetMapping(path = "/estadoMundial", produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody Mundial getTotalMundial(){
-		
-		LOGGER.log(Level.INFO, "Consulta mundial");
-		
-		RestTemplate restTemplate = new RestTemplate();
-	    ResponseEntity<String> call= restTemplate.getForEntity("https://api.covid19api.com/world/total" ,String.class);
-	    Mundial response = new Mundial();
-		Gson gson = new Gson();
-        Mundial estado = gson.fromJson(call.getBody().toLowerCase(), Mundial.class);
-        response.setTotalConfirmed(estado.getTotalConfirmed());
-        response.setTotalDeaths(estado.getTotalDeaths());
-        response.setTotalRecovered(estado.getTotalRecovered());
+    @GetMapping(path = "/estadoMundial", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    Mundial getTotalMundial() {
 
-		return response;		
-	}
+        LOGGER.log(Level.INFO, "Consulta mundial");
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> call = restTemplate.getForEntity("https://api.covid19api.com/world/total", String.class);
+        Mundial response = new Mundial();
+        Gson gson = new Gson();
+
+/**        String body = Optional.ofNullable(call.getBody()).orElse("");
+ **/
+        String body = call.getBody();
+        if (body != null) {
+            Mundial estado = gson.fromJson(body.toLowerCase(), Mundial.class);
+            response.setTotalConfirmed(estado.getTotalConfirmed());
+            response.setTotalDeaths(estado.getTotalDeaths());
+            response.setTotalRecovered(estado.getTotalRecovered());
+
+            return response;
+        }
+        return response;
+    }
 }
--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -1,13 +1,21 @@
 package com.devopsusach2020;
 
+import com.devopsusach2020.model.Pais;
+import com.devopsusach2020.rest.RestData;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 class DevOpsUsach2020ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        RestData restData = new RestData();
+        Pais response = restData.getData("HOLA devops");
+        assertEquals("Mensaje Recibido: HOLA devops", response.getMensaje());
+    }
 
 }
