420719d22247293bb79aa4ebbd32370b039f9814
Jaime Paredes <jparedes@imaginex.cl>
2022-11-08 23:48:53 -0300

    Add: simple test

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -1,13 +1,21 @@
 package com.devopsusach2020;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+
 class DevOpsUsach2020ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Value("${1}")
+  Integer numberOne;
+  @Test
+  void test(){
+    assertEquals(numberOne, 1);
+  }
 }
